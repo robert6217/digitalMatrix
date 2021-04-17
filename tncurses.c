7e76ab6b8d67a70233ac4cbd9f0ccaeb8711e072
robert6217 <a22780911@gmail.com>
2021-04-18 01:52:43 +0800

    ncurses example

--- tncurses.c
+++ tncurses.c
@@ -1,6 +1,20 @@
+#include <ncurses.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char *argv) {
-    printf("hello workd\n");
+int main(int argc, const char* argv[]) {
+    // printf("hello workd\n");
+    initscr();
+    raw();
+    noecho();
+    curs_set(0);
+
+    char* c = "Hello, World!";
+
+    mvprintw(LINES / 2, (COLS - strlen(c)) / 2, c);
+    refresh();
+
+    getch();
+    endwin();
     return 0;
 }
