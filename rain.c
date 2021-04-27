a1a37c45c0fbf381ba85c847aaa89b0ac70aeb20
robert6217 <a22780911@gmail.com>
2021-04-28 00:36:28 +0800

    add color

--- rain.c
+++ rain.c
@@ -6,7 +6,6 @@
 static unsigned int numOfRain;
 static unsigned int maxSign = 126, minSign = 33;
 
-
 int initRain() {
     time_t t;
     srand((unsigned)time(&t));
@@ -23,6 +22,11 @@ int main() {
     curs_set(FALSE);
     int x = rand() % 10, i = 0;
 
+    /*  set color  */
+    start_color();
+    use_default_colors();
+    init_pair(3, COLOR_GREEN, -1);
+
     while (1) {
         usleep(100000);
 
@@ -33,7 +37,10 @@ int main() {
         }
         int n = 0;
         for (n = 0; n < 10; n++) {
-            mvprintw(i + n, x, rand() % (maxSign - minSign) + minSign);
+            attron(COLOR_PAIR(3));
+            mvaddch(i + n, x, rand() % (maxSign - minSign) + minSign);
+            attroff(COLOR_PAIR(3));
+            // mvprintw(i + n, x, rand() % (maxSign - minSign) + minSign);
         }
         refresh();
         i++;
