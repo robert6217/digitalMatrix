123625b99acced8521307496ddd38dd47e9e4aed
robert6217 <a22780911@gmail.com>
2021-05-01 00:55:16 +0800

    - pthread

--- rain.c
+++ rain.c
@@ -1,11 +1,27 @@
 #include <ncurses.h>
-#include <pthread.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #define RATE 50
 static unsigned int numOfRain;
 static unsigned int maxSign = 126, minSign = 33;
+int initColor();
+int initCurse();
+int initRains();
+void freerain();
+void *raining(void *arg);
+typedef struct rain {
+    unsigned short length, col;
+    short row;
+    unsigned short frames_per_row;
+} Rain;
+static Rain *rains;
+
+void freerain() {
+    free(rains);
+    endwin();
+    return;
+}
 
 void *raining(void *arg) {
     int *input = (int *)arg;
@@ -21,62 +37,66 @@ void *raining(void *arg) {
         mvaddch(i + n, x, rand() % (maxSign - minSign) + minSign);
         attroff(COLOR_PAIR(8));
     }
+
     return 0;
 }
-
-int initRain() {
+int initRains() {
     time_t t;
     srand((unsigned)time(&t));
     unsigned int screenArea = LINES * COLS;
     numOfRain = screenArea / RATE;
-    // pthread;
+    rains = malloc(sizeof(struct rain) * numOfRain);
+
+    if (!rains) {
+        perror("Out of memory\n");
+        exit(1);
+    }
 
     return 0;
 }
-
-int main() {
-    pthread_t t[numOfRain];
-    initRain();
-    initscr();
-    noecho();
-    curs_set(FALSE);
-    int x = rand() % 10, i = 0;
-
-    /*  set color  */
+int initColor() {
     start_color();
     use_default_colors();
     init_pair(3, COLOR_GREEN, -1);
     init_pair(8, COLOR_WHITE, -1);
+    return 0;
+}
+int initCurse() {
+    initscr();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+    noecho();
+    cbreak();
+    atexit(freerain);
+    nodelay(stdscr, TRUE);
+    initColor();
+    initRains();
+    return 0;
+}
 
-    // /*  set sleep time  */
-    // struct timespec ts;
-    // ts.tv_sec = 200 / 1000;
-    // ts.tv_nsec = (20 % 1000) * 1000000;
+int main() {
+    initCurse();
+    int x = rand() % 10, i = 0;
+
+    /*  set sleep time  */
+    struct timespec ts;
+    ts.tv_sec = 200 / 1000;
+    ts.tv_nsec = (20 % 1000) * 1000000;
 
     while (1) {
-        // nanosleep(&ts, &ts1);
         refresh();
         usleep(100000);
         clear();
+        // nanosleep(&ts, NULL);
         if (i == LINES) {
             x = rand() % COLS;
             i = 0;
         }
         int input[2] = {i, x};
-        int threadn = 0;
-        // raining(input);
-        // pthread_create(&t[threadn], NULL, raining, (void *)input);
-        // pthread_join(t[threadn], NULL);
-        for (; threadn < numOfRain; threadn++) {
-            pthread_create(&t[threadn], NULL, raining, (void *)input);
-        }
-        for(; threadn < numOfRain; threadn++){
-            pthread_join(t[threadn], NULL);
-
-        }
+        raining(input);
 
         i++;
     }
-    endwin();
+
     return 0;
 }
